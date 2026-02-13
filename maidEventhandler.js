b75c03ba9b8df75adfe200b0acf691cb7d1e9d36
seto-machi <taikozelda@yahoo.co.jp>
2026-02-14 00:05:21 +0900

    改善案を受けてのコード更新

---
+++ maidEventhandler.js
@@ -0,0 +1,52 @@
+import {
+    background_change,
+    background_transfar,
+    beer_classname,
+    bg,
+    food_img,
+    poteto_classname,
+    txt,
+    koucha_classname
+} from "./helper.js";
+import { btnbox } from "./home.js";
+
+export const teaButtonHandler = (before_serif, clazz, chara, after_serif) => {
+    txt.innerHTML = before_serif;
+    btnbox.innerHTML = "";
+    setTimeout(() => {
+        background_transfar();
+        txt.innerHTML = "5分後";
+        setTimeout(() => {
+            bg.innerHTML = food_img(clazz, chara, after_serif);
+            background_change(koucha_classname);
+        }, 3000);
+    }, 3000);
+
+}
+
+
+export const potatoButtonHandler = (before_serif, clazz, chara, after_serif) => {
+    txt.innerHTML = before_serif;
+    btnbox.innerHTML = "";
+    setTimeout(() => {
+        background_transfar();
+        txt.innerHTML = "15分後";
+        setTimeout(() => {
+            bg.innerHTML = food_img(clazz, chara, after_serif);
+            background_change(poteto_classname);
+        }, 3000);
+    }, 3000);
+}
+
+export const beerButtonHandler = (before_serif, clazz, chara, after_serif) => {
+    txt.innerHTML = before_serif;
+    btnbox.innerHTML = "";
+    setTimeout(() => {
+        background_transfar();
+        txt.innerHTML = "3分後";
+        setTimeout(() => {
+            bg.innerHTML = food_img(clazz, chara, after_serif);
+            background_change(beer_classname);
+        }, 3000);
+    }, 3000);
+}
--- rikka.js
+++ rikka.js
@@ -1,30 +1,38 @@
-import {
-    menu,
-    rikka_classname,
-    select_class,
-    txt,
-} from "./helper.js";
-import { btnbox } from "./home.js";
-import { beerButtonHandler, potatoButtonHandler, teaButtonHandler } from "./rikkaEventHandlers.js";
+import { menu, thumb_berta, thumb_xiaolan, txt } from "./helper.js";
+import { btnbox } from "./home.js"
+import { beerButtonHandler, potatoButtonHandler, teaButtonHandler } from "./maidEventhandler.js";
 
 export function rikka_event() {
     txt.innerHTML = "いらっしゃいませ。何をご注文ですか？";
+    const rikka_classname = "rikka";
     btnbox.classList.add(rikka_classname);
 
     menu();
 
     const rikka = document.querySelectorAll(`.${rikka_classname} button`);
-    const thumb_berta = select_class(".berta");
-    const thumb_xiaolan = select_class(".xiaolan");
     thumb_berta.innerHTML = "";
     thumb_xiaolan.innerHTML = "";
 
-    const teaButton = rikka[0];
-    const potatoButton = rikka[1];
-    const beerButton = rikka[2];
+    const teaButton_rikka = rikka[0];
+    const potatoButton_rikka = rikka[1];
+    const beerButton_rikka = rikka[2];
 
+    const rikka_koucha_before = "紅茶ですね。しばしお待ちくださいませ";
+    const rikka_poteto_before = "ポテトですね。今から揚げますのでお時間がかかります。しばしお待ちくださいませ";
+    const rikka_beer_before = "ビールですね。すぐにお持ちします";
+    const rikka_photo_clazz = 'rikka';
+    const rikka_photo_address = "rr.png";
+    const rikka_serif_after = (menu) => {
+        return `お待たせいたしました。${menu}でございます。ごゆるりとおくつろぎくださいませ。`
+    }
 
-    teaButton.addEventListener("click", teaButtonHandler);
-    potatoButton.addEventListener("click", potatoButtonHandler);
-    beerButton.addEventListener("click", beerButtonHandler);
+    teaButton_rikka.addEventListener('click', function () {
+        teaButtonHandler(rikka_koucha_before, rikka_photo_clazz, rikka_photo_address, rikka_serif_after("紅茶"))
+    });
+    potatoButton_rikka.addEventListener('click', function () {
+        potatoButtonHandler(rikka_poteto_before, rikka_photo_clazz, rikka_photo_address, rikka_serif_after("ポテト"))
+    });
+    beerButton_rikka.addEventListener('click', function () {
+        beerButtonHandler(rikka_beer_before, rikka_photo_clazz, rikka_photo_address, rikka_serif_after("ビール"))
+    });
 }
